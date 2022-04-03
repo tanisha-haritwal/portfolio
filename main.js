9b169f1be232184e0b00a43af8d1fdc47a61767b
tanisha-haritwal <86518130+tanisha-haritwal@users.noreply.github.com>
2022-04-03 16:31:14 +0530

    Project 108
    ...

--- main.js
+++ main.js
@@ -6,6 +6,26 @@ ml5.classifier(model.json , "modelReady()")
 function modelReady(){
 classifier.classify(gotResults)
 }
-function gotResults(){
-console.log(gotResults)
+function gotResults(error, results){
+if(error){
+console.error(error);
+}
+else{
+console.log(results);
+random_number_r= Math.floor(Math.random()*255)+1;
+random_number_g= Math.floor(Math.random()*255)+1;
+random_number_b= Math.floor(Math.random()*255)+1;
+dog=0;
+cat=0;
+cow=0;
+tiger=0;
+document.getElementById("id_of_element")
+document.getElementById("result_count").style
+document.getElementById("result_count").style.color
+img1= document.getElementById("dog-barking.gif");
+img2= document.getElementById("cat-meowing.gif");
+img3= document.getElementById("cow-mooing.gif");
+img4= document.getElementById("tiger-roaring.gif");
+
+}
 }
