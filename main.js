0c1377a5a2cc6d4a995eaed7397553cb04ac7260
tanisha-haritwal <86518130+tanisha-haritwal@users.noreply.github.com>
2022-04-10 16:10:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
-<link rel="preconnect" href="https://fonts.googleapis.com"></link>
-function startClassification(){
-navigator.mediaDevices.getUserMedia ({ audio: true});
-ml5.classifier(model.json , "modelReady()")
-}
-function modelReady(){
-classifier.classify(gotResults)
-}
-function gotResults(error, results){
-if(error){
-console.error(error);
-}
-else{
-console.log(results);
-random_number_r= Math.floor(Math.random()*255)+1;
-random_number_g= Math.floor(Math.random()*255)+1;
-random_number_b= Math.floor(Math.random()*255)+1;
-dog=0;
-cat=0;
-cow=0;
-tiger=0;
-document.getElementById("id_of_element")
-document.getElementById("result_count").style
-document.getElementById("result_count").style.color
-img1= document.getElementById("dog-barking.gif");
-img2= document.getElementById("cat-meowing.gif");
-img3= document.getElementById("cow-mooing.gif");
-img4= document.getElementById("tiger-roaring.gif");
+Webcam.set({
+width:350,
+height:300,
+img_format:'png',
+png:90
+});
 
+camera= document.getElementById("camera");
+Webcam.attach('camera');
+
+function take_snapshot(){
+Webcam.snap(function (data_uri){
+document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+})
 }
-}
+console.log('ml5 version:', ml5.version);
+classifier=imageClassifier('https://teachablemachine.withgoogle.com/models/uMptNA6FI/model.json', modelLoaded);
