09ff4744d230a640c02b0885345c7fe2c085ef49
tanisha-haritwal <86518130+tanisha-haritwal@users.noreply.github.com>
2022-05-01 15:59:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,18 +6,17 @@ canvas=createCanvas(640,480);
 canvas.position(110,250);
 video=createCapture(VIDEO);
 video.hide();
-tint_color="";
 }
 
 function draw(){
 image(video,0,0,640,480);
-tint(tint_color);
+circle(30, 30, 70);
+circle(610, 30, 70)
+circle(610, 445, 70)
+circle(30, 445, 70) 
 }
-
+    
 function take_snapshot(){
-save('filter_img.png');
+save('picture_img.jpg');
 }
-
-function filter_tint(){
-tint_color=document.getElementById("color_name").value;
-}
+    
