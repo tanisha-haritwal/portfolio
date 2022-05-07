ac051b742da1675e2b96a4fd63ec10b157dc3c54
tanisha-haritwal <86518130+tanisha-haritwal@users.noreply.github.com>
2022-05-07 11:10:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,21 +2,13 @@ function preload(){
 }
 
 function setup(){
-canvas=createCanvas(640,480);
-canvas.position(110,250);
-video=createCapture(VIDEO);
-video.hide();
+canvas=createCanvas(300, 300);
+canvas.center();
 }
 
 function draw(){
-image(video,0,0,640,480);
-circle(30, 30, 70);
-circle(610, 30, 70)
-circle(610, 445, 70)
-circle(30, 445, 70) 
 }
-    
-function take_snapshot(){
-save('picture_img.jpg');
+
+function onclick(){
+save()
 }
-    
