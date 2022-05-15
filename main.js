399726671c9a8f5e1f3b9e51115d6be01a02fa4e
tanisha-haritwal <86518130+tanisha-haritwal@users.noreply.github.com>
2022-05-15 16:01:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,13 +2,30 @@ function preload(){
 }
 
 function setup(){
-canvas=createCanvas(300, 300);
-canvas.center();
+canvas=createCanvas(640,480);
+canvas.position(110,250);
+video=createCapture(VIDEO);
+video.hide();
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 
 function draw(){
+image(video,0,0,640,480);
+}
+    
+function take_snapshot(){
+save('clown_nose_img.jpg');
 }
 
-function onclick(){
-save()
+function gotPoses(results){
+if(results.length > 0){
+console.log(results);
+console.log("nose x =" + results[0].pose.nose.x);
+console.log("nose y =" + results[0].pose.nose.y);
+}
+}
+
+function modelLoaded(){
+console.log('modelLoaded');
 }
